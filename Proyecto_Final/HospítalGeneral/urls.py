fc52a8d90df9f341a432cdbecf61ca1f1ff6c531
cufe978 <facuiperez97@gmail.com>
2022-03-26 05:47:01 -0300

    change in line 2 to "Proyecto_Final.HospítalGeneral

--- Proyecto_Final/HospítalGeneral/urls.py
+++ Proyecto_Final/HospítalGeneral/urls.py
@@ -1,10 +1,9 @@
 from django.urls import include, path
-from HospítalGeneral import views
-
+from Proyecto_Final.HospítalGeneral import views
 
 
 urlpatterns = [
-    path('index', views.index, name = 'Index'),
-    path('paciente', views.paciente, name = 'Paciente'),
-    path('doctor', views.doctor, name = 'Doctor')    
+    path('index', views.index, name ='Index'),
+    path('paciente', views.paciente, name ='Paciente'),
+    path('doctor', views.doctor, name ='Doctor')
 ]
